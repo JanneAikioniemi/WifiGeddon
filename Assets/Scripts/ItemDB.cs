f04ca1af8fe33be88aaac2c833add315044dce44
Janne Aikioniemi <janne.aikioniemi@siili.com>
2018-01-27 10:48:12 +0200

    WIP.

---
+++ Assets/Scripts/ItemDB.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDB 
+{
+    public class Item
+    {
+        protected float price;
+
+        public virtual void SetPrice(float p)
+        {
+            price = p;
+        }
+    }
+
+    public class FoodItem : Item
+    {
+
+    }
+
+    
+
+    public class SabotageItems : Item
+    {
+
+    }
+
+
+}
---
+++ Assets/Scripts/MenuActions/ActionBuyItem.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionBuyItem : MenuAction 
+{
+    public override void Act()
+    {
+        base.Act();
+
+    }
+}
